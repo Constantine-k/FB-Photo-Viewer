96a7abf1d45d6ec133e1f43a9771832e692c50b7
Konstantin Konstantinov <konstantin.k.bm@gmail.com>
2017-09-15 14:36:26 +0300

    Add Facebook login button

---
+++ FacebookPhotoViewer/LoginViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  FacebookPhotoViewer
+//
+//  Created by Konstantin Konstantinov on 9/14/17.
+//  Copyright © 2017 Konstantin Konstantinov. All rights reserved.
+//
+
+import UIKit
+
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /// Facebook login button
+        let loginButton = FBSDKLoginButton()
+        loginButton.center = view.center
+        view.addSubview(loginButton)
+        
+        loginButton.delegate = self
+        
+        if (FBSDKAccessToken.current() != nil) {
+            // User is logged in, do work such as go to next view controller.
+            print("User is logged in")
+        }
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        print("Completed login")
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+
+target 'FacebookPhotoViewer' do
+  use_frameworks!
+  
+  pod 'FBSDKCoreKit', '~> 4.26'
+  pod 'FBSDKLoginKit', '~> 4.26'
+end
